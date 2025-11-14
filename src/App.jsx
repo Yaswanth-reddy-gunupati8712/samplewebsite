53d7102624b2baa1c6d4cf1f147b9f6958039cfd
yaswanth <gunupatiyaswanth321@gmail.com>
2025-11-14 14:06:21 +0530

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import img from './img.svg';
 import './App.css'
+import './Nav.css'
+import Home from './Home'
+import Logo from './Logo'
+import Heading from './Heading'
+import Login from './Login'
+import Nav from './Nav'
 
 function App() {
 
   return (
     <>
-    <h1>hvchjsjnx</h1>
+      <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        {/* <img src={img} alt="img" /> */}
+        <Route path='/Logo' element={<Logo/>} />
+        <Route path='/img' element={<img />} />
+        <Route path='/Heading' element={<Heading/>} />
+        <Route path='/Login' element={<Login/>} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
